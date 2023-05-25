d4bea9c7022ef7459425b6630afee4a1e5c7c40a
Danilo Medas <medas.danilo@gmail.com>
2023-05-25 20:57:53 +0200

    fixed most of the things i wrote in the comments of battle_scene.gd

--- Battle/battle_scene.gd
+++ Battle/battle_scene.gd
@@ -25,11 +25,12 @@ func init_monsters(player : Monster = player, enemy : Monster = enemy) -> void:
 	$Player/Name.text = str(player.nickname) + " lvl." + str(player.level)
 	$Player/HP.max_value = player.hp
 
-#NULL the move array could be empty or an entry could be empty
 func init_moves(player : Monster = player) -> void:
-	# for loop to initialize all four moves
+	if player.moves == null:
+		return
 	for i in range(0,4):
-		MovesBox.get_node("Move" + str(i)).text = player.moves[i].nickname
+		if player.moves[i] != null:
+			MovesBox.get_node("Move" + str(i)).text = player.moves[i].nickname
 
 func start_encounter() -> void:
 	await self.ui_select_pressed
@@ -38,13 +39,15 @@ func start_encounter() -> void:
 	$DescriptionBox.visible = false
 	$AttackMenu.visible = true
 
-#OPTIMIZE I have to find a way to remove this check or see if it impacts in the performance or is irrelevant
 func _process(delta) -> void:
-	if Input.is_action_pressed("ui_select"):
+#OPTIMIZE Looks like it's irrelevant for the performance if I use is_action_just_pressed()
+	if Input.is_action_just_pressed("ui_select"):
 		emit_signal("ui_select_pressed")
 
-#NULL fare controllo se la mossa esista
 func compute_attack(attacker : Monster, attacked : Monster, move : Move) -> void:
+	if move == null:
+		print("Move doesn't exist")
+		return
 	if rng.randi_range(0,100) <= move.accuracy:
 		$DescriptionBox.text = attacker.nickname + " used " + move.nickname + "!"
 		$DescriptionBox.visible = true
@@ -69,35 +72,30 @@ func damage_calculation(move : Move, attacker : Monster = player, attacked : Mon
 		false:
 			register_damage(enemy, player, third_par, $Player/HP)
 
-#OPTIMIZE set_value_no_signal() to avoid calling a signal whenever health changes?
 func register_damage(attacker : Monster, attacked : Monster, damage : int, health_bar : ProgressBar) -> void:
-	var tween = get_tree().create_tween() #OPTIMIZE after the functions returns is this node removed?
+	var tw : Tween = create_tween()
 	attacked.hp -= damage
-	tween.tween_property(health_bar, "value", health_bar.value - damage, HP_BAR_SPEED) # this works properly
+#I don't know if tween_property emits signal when it changes healthbar value, but I don't think it is much relevant
+	tw.tween_property(health_bar, "value", health_bar.value - damage, HP_BAR_SPEED)
 	await get_tree().create_timer(HP_BAR_SPEED).timeout
-#	tween.interpolate_value(health_bar.value, damage, 2, 3, tween.TRANS_LINEAR, tween.EASE_OUT) how does this work?
-	# attacked.hp -= damage
 	if attacked.hp <= 0:
 		end_battle(attacker)
 	elif attacker == enemy:
 		$AttackMenu.visible = true
 
 func end_battle(winner : Monster) -> void:
-	var tween = get_tree().create_tween()
+	var tw : Tween = create_tween()
 	$AttackMenu.visible = false
 	$DescriptionBox.text = winner.nickname + " won the battle!"
 	$DescriptionBox.visible = true
 	
-	tween.tween_property($BattleMusic, "volume_db", -80, 3)
+	tw.tween_property($BattleMusic, "volume_db", -80, 3)
 	await get_tree().create_timer(HP_BAR_SPEED).timeout
 	self.visible = false
 	
 func enemy_turn() -> void:
 	$AttackMenu.visible = false
 	compute_attack(enemy, player, enemy.moves[rng.randi_range(0,3)])
-
-	#OPTIMIZE I don't know why I need this timer, because the enemy turn is already awaited
-	await get_tree().create_timer(HP_BAR_SPEED).timeout
 	$DescriptionBox.visible = false
 
 #OPTIMIZE Maybe can be optimized?
@@ -124,17 +122,16 @@ func attack_missed(attacker : Monster) -> void:
 	await get_tree().create_timer(HP_BAR_SPEED).timeout
 
 func animate_monster(monster : Monster) -> void: #ADD animation in input. Now the animation is the same for every monster
-	var tween : Tween = create_tween() #OPTIMIZE after the functions returns is this node removed?
-	#BUG I don't know why the second tween starts before the first ends
+	var tw : Tween = create_tween()
 	var position : Vector2
 	if monster == player:
 		position = $PlayerSprite.position
-		tween.tween_property($PlayerSprite, "position", position + Vector2(50,0), 0.2)
+		tw.tween_property($PlayerSprite, "position", position + Vector2(50,0), 0.2)
 		position += Vector2(50,0)
-		tween.tween_property($PlayerSprite, "position", position - Vector2(50,0), 0.3)
+		tw.tween_property($PlayerSprite, "position", position - Vector2(50,0), 0.3)
 	else:
 		position = $EnemySprite.position
-		tween.tween_property($EnemySprite, "position", position - Vector2(50,0), 0.2)
+		tw.tween_property($EnemySprite, "position", position - Vector2(50,0), 0.2)
 		position -= Vector2(50,0)
-		tween.tween_property($EnemySprite, "position", position + Vector2(50,0), 0.3)
+		tw.tween_property($EnemySprite, "position", position + Vector2(50,0), 0.3)
 	await get_tree().create_timer(1).timeout
